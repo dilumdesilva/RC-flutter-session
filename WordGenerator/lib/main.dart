eb0dd1b08091cdc651607294a1679dcec8b88fae
Dilum De Silva <dilumdesilva.info@gmail.com>
2020-11-15 06:03:28 +0530

    add: word generator

---
+++ WordGenerator/lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // #docregion build
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Random Word Generator',
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final _customFont = TextStyle(fontSize: 18.0);
+  final _saved = Set<WordPair>();
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(15),
+        itemBuilder: (_context, i) {
+          if (i.isOdd) {
+            return Divider();
+          }
+
+          final int index = i ~/ 2; // 1,2,3 -> 0,1,1,2
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _customFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          alreadySaved ? _saved.remove(pair) : _saved.add(pair);
+        });
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Startup Name Generator"),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+}
